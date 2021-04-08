37939a11118c6cebfd90564fddc7601343e3f369
gerryron <gerryron02@gmail.com>
2021-04-08 20:10:30 +0700

    menambahkan fitur registrasi

--- UAS/functions.php
+++ UAS/functions.php
@@ -15,4 +15,44 @@
     return $rows;
   }
 
+  function registrasi($data) {
+    global $conn;
+    $provinsi = $data["provinsi"];
+    $kota = $data["kota"];
+    $kecamatan = $data["kecamatan"];
+    $jenis_faskes = $data["jenis_faskes"];
+    $faskes = $data["faskes"];
+    $nik = $data["nik"];
+    $nama = $data["nama"];
+    $gender = $data["gender"];
+    $umur = $data["umur"];
+    $tanggalLahir = $data["tanggal_lahir"];
+    $nohp = $data["nohp"];
+    $alamat = $data["alamat"];
+    $password = mysqli_real_escape_string($conn, $data["password"]);
+    $confirmPassword = mysqli_real_escape_string($conn, $data["confirm-password"]);
+
+    $result = mysqli_query($conn, "SELECT nomer_hp FROM pasien WHERE nomer_hp = '$nohp'");
+    if (mysqli_fetch_assoc($result)){
+      echo "<script>
+              alert('Nomer HP sudah terdaftar di sistem.');
+           </script>";
+      return false;
+    }
+
+    if ($password != $confirmPassword) {
+      echo "<script>
+              alert('Konfirmasi Password tidak sesuai');
+           </script>";
+      return false;
+    }
+
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    mysqli_query($conn, "INSERT INTO pasien VALUES 
+              (NULL, '$provinsi', '$kota', '$kecamatan', '$jenis_faskes', '$faskes', '$nik', '$nama', '$gender', '$umur', '$tanggalLahir', '$nohp', '$alamat', '$password')");
+
+    return mysqli_affected_rows($conn);
+  }
+
 ?>
